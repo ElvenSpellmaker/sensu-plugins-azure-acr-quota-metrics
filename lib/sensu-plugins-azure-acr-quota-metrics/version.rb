2d7041b95a91bb094364ca6cc06add9b711e9069
Jack Blower <Jack.Blower@veinteractive.com>
2019-05-07 11:51:53 +0100

    Updates dependencies so the gem builds correctly

--- lib/sensu-plugins-azure-acr-quota-metrics/version.rb
+++ lib/sensu-plugins-azure-acr-quota-metrics/version.rb
@@ -2,7 +2,7 @@ module SensuPluginsAzureAcrQuotaMetrics
   class Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
+    PATCH = 2
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
